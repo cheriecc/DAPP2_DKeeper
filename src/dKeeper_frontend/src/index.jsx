10720f30f8cb81750d663e35dc8d24e003db35cc
Cherie C Sun <chane.c.sun@gmail.com>
2023-02-16 01:50:02 +0000

    update a lot

---
+++ src/dKeeper_frontend/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM, {render} from "react-dom";
+import App from "./components/App";
+import { dKeeper_backend } from "../../declarations/dKeeper_backend";
+
+render(<App />, document.getElementById("root"));
